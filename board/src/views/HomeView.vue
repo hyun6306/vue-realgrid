a81dad49f2032980d2ef1480cd1db63637b1ced6
yhcho <hyun6306@naver.com>
2023-04-24 16:34:24 +0900

    RealGrid Board push
    RealGrid Board push

---
+++ board/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+<h1>Hello</h1>
+</template>
+
+<script>
+import apiBoard from '@/api/board';
+
+export default {
+    mounted() {
+        apiBoard.getArticle(1)
+        .then(function (response) {
+            console.log('getArticle', response);
+        })
+        .catch(function (e) {
+            console.log(e);
+        });
+
+        //getArticles 데이터를 배열로 목록을 가져오는 API
+        apiBoard.getArticles(0)
+          .then(function (response) {
+              console.log('getArticles', response);
+          })
+          .catch(function (e) {
+            console.log(e);
+          });
+
+        apiBoard.postArticle('id','title','body')
+        .then(function (response){
+          console.log('postAticle', response);          
+        })
+        .catch(function (e) {
+          console.log(e);
+        });
+    },
+}
+</script>
